1618207d555cfc4a4248544f9152f04b4f6e79ac
Vansh Pratap Singh <vanshps987@gmail.com>
2026-07-04 00:58:14 +0530

    day 3 tasks

---
+++ app/src/main/java/com/altrodav/friendcircle/learning/DayThree.kt
@@ -0,0 +1,25 @@
+package com.altrodav.friendcircle.learning
+
+data class Friend(val id:String,var name:String, var email:String, var password:String,var age:Int){
+    fun addFriend(friend:Friend){
+        println("Friend added: ${friend.name}")
+    }
+}
+
+fun main(){
+//    I was practising with concepts such as data classes and other collection types
+    val vinnie= Friend("123","Vansh","vans123@protonmail.com","Pass%358",21);
+    val anku= Friend("256","Ankit","ankit23@protonmail.com","Pass%358",17);
+    val sak=Friend("768","Sakshi","sakshi@protonmail.com","Pass%358",22);
+    val friendsList=listOf<Friend>(
+        vinnie,anku,sak
+    )
+//    for(friend in friendsList){
+//        println(friend.toString())
+//    }
+    val names=friendsList.filter { it.age>18 }.map { it.name };
+    val friendsMap:Map<String,Friend> = friendsList.associateBy { it.id }
+    println(friendsMap["123"])
+    println(names)
+}
+
