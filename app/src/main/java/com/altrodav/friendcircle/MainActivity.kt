4c83b0deba163f70d2a1617963a81ffba588a211
Vansh Pratap Singh <vanshps987@gmail.com>
2026-07-04 21:09:47 +0530

    coil dependency installed

--- app/src/main/java/com/altrodav/friendcircle/MainActivity.kt
+++ app/src/main/java/com/altrodav/friendcircle/MainActivity.kt
@@ -4,24 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.altrodav.friendcircle.ui.theme.FriendCircleTheme
-
+import coil.compose.AsyncImage
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FriendCircleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
+                Box(modifier= Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    AsyncImage(
+                        model = "https://www.pngitem.com/pimgs/m/13-132003_android-robot-sideview-character-android-tutorial-hd-png.png",
+                        contentDescription = "Test Image Loading"
                     )
                 }
             }
